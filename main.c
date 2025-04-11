5b37054f00ea83bc3a45f59d087145294197114e
Purendra Srivastava <32216012+Purendra1@users.noreply.github.com>
2025-04-11 19:13:05 +0530

    Defining boundary and creating mathematical entities to support ball travel and collisions

--- main.c
+++ main.c
@@ -6,9 +6,31 @@ struct Paddle {
     int ys; //0-25
 }p1, p2;
 
+struct AngleIR {
+    int incx;
+    int incy;
+}zero, thirty, fourtyfive, sixty, ninety;
+
+struct Ball {
+  int x;
+  int y;
+  int chd;
+  int cvd;
+  AngleIR angle;
+}b;
+
+/**
+angle  incx  incy
+0       -1    0
+90       0    -1
+30       3    1
+45       1    1
+60       1    3
+*/
+
 void initPaddles() {
-    p1.x=0;
-    p2.x=119;
+    p1.x=1;
+    p2.x=118;
 
     p1.ys=12;
     p2.ys=12;
@@ -16,8 +38,10 @@ void initPaddles() {
 
 void draw();
 int isPaddleHere(struct Paddle*,int,int);
-int shouldPrintPixel(int,int);
+char getPixelToPrint(int,int);
 void processInput(int c);
+int isBoundary(int,int);
+int isBall(int,int);
 
 int main() {
     initPaddles();
@@ -39,26 +63,26 @@ Numpad 8 - Player 2 Paddle Up
 Numpad 5 - Player 2 Paddle Down
 */
 void processInput(int c) {
-    if (c=='w')if(p1.ys>0)p1.ys--;
-    if (c=='s')if(p1.ys<25)p1.ys++;
-    if (c=='8')if(p2.ys>0)p2.ys--;
-    if (c=='5')if(p2.ys<25)p2.ys++;
+    if (c=='w')if(p1.ys>1)p1.ys--;
+    if (c=='s')if(p1.ys<24)p1.ys++;
+    if (c=='8')if(p2.ys>1)p2.ys--;
+    if (c=='5')if(p2.ys<24)p2.ys++;
 }
 
 
 void draw() {
     for(int y=0;y<30;y++) {
         for(int x=0;x<120;x++) {
-            if(shouldPrintPixel(x,y))printf("*");
-            else printf(" ");
+            printf("%c",getPixelToPrint(x,y));
         }
     }
 }
 
-int shouldPrintPixel(int x, int y) {
-    if (isPaddleHere(&p1,x,y)||isPaddleHere(&p2,x,y)) return 1;
-    if (x==59&&y==14)return 1;
-    return 0;
+char getPixelToPrint(int x, int y) {
+    if (isPaddleHere(&p1,x,y)||isPaddleHere(&p2,x,y)) return '*';
+    if (isBall(x,y))return 'O';
+    if (isBoundary(x,y)) return '#';
+    return ' ';
 }
 
 int isPaddleHere(struct Paddle* p, int x, int y) {
@@ -66,4 +90,17 @@ int isPaddleHere(struct Paddle* p, int x, int y) {
     if ((p->ys)>y)return 0;
     if ((p->ys)+5<=y)return 0;
     return 1;
+}
+
+int isBoundary(int x, int y) {
+    if (y==0||y==29)return 1;
+    if (y>=1&&y<=7||y<=29&&y>=22) {
+        if(x==0||x==119)return 1;
+    }
+    return 0;
+}
+
+int isBall(int x,int y) {
+    if (x==59&&y==15)return 1;
+    return 0;
 }
